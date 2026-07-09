ae7fab3b7be3cb0f7bd0bda29c5dc203ffed1103
epsilonthehome1 <epsilonthehome@gmail.com>
2026-07-09 11:42:11 +0200

    Fix alert message and formatting in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelector(".btn").onclick=function(){
+document.querySelector(".btn").onclick = function(){
 
-alert("Welcome to KawaHub 🚀\n\nThe future of African business begins today!");
+alert("🚀 Welcome to KawaHub!\n\nThe future of African business starts here.");
 
 }
