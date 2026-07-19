268c120b03724c61c01bf7a25bff4855dd1bdcf8
epsilonthehome1 <epsilonthehome@gmail.com>
2026-07-19 09:28:29 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBNV8k3tkgP0m0jAwct5Nk03uBCrM_RSkQ",
   authDomain: "kawahub.firebaseapp.com",
@@ -15,7 +10,8 @@ const firebaseConfig = {
   appId: "1:231433018244:web:682ea51aea88d00d8fdbd1",
   measurementId: "G-8BQ1YFP326"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+export { auth };
