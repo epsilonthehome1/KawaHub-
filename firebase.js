bdb833b6b274a733def703d444ea3d14bdf9055d
epsilonthehome1 <epsilonthehome@gmail.com>
2026-07-19 18:49:18 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNV8k3tkgP0m0jAwct5Nk03uBCrM_RSkQ",
@@ -10,8 +13,11 @@ const firebaseConfig = {
   appId: "1:231433018244:web:682ea51aea88d00d8fdbd1",
   measurementId: "G-8BQ1YFP326"
 };
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export { auth };
+const db = getFirestore(app);
+
+export { auth, db };
