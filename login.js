cda872006fe9363b6fd7dbf299e8701b3701a167
epsilonthehome1 <epsilonthehome@gmail.com>
2026-07-19 18:17:38 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,33 @@
+import { auth } from "./firebase.js";
+
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e)=>{
+
+e.preventDefault();
+
+const email=document.getElementById("email").value;
+
+const password=document.getElementById("password").value;
+
+try{
+
+await signInWithEmailAndPassword(auth,email,password);
+
+alert("Welcome back!");
+
+window.location.href="dashboard.html";
+
+}
+
+catch(error){
+
+alert(error.message);
+
+}
+
+});
