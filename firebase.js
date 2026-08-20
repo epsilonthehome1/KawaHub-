1a4a8b1a49f13e32515de33590acc546987d7618
epsilonthehome1 <epsilonthehome@gmail.com>
2026-08-20 12:14:05 +0200

    Add Firebase Storage to firebase.js

--- firebase.js
+++ firebase.js
@@ -4,6 +4,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBNV8k3tkgP0m0jAwct5Nk03uBCrM_RSkQ",
   authDomain: "kawahub.firebaseapp.com",
@@ -14,10 +17,14 @@ const firebaseConfig = {
   measurementId: "G-8BQ1YFP326"
 };
 
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-export { auth, db };
+const storage = getStorage(app);
+
+
+export { auth, db, storage };
